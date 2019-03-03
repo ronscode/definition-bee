e6305bce4dfef823c652c8a775b94d4ed7ce6b0d
Ron Cagle <ron@atlantashirt.com>
2019-03-02 23:13:10 -0500

    Added red strikes

--- definition-bee/src/components/Play.jsx
+++ definition-bee/src/components/Play.jsx
@@ -2,18 +2,39 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { Jumbotron, Container, Row, Col, Image, Button } from "react-bootstrap";
 import "./Home.css";
+import PlayCountdown from "./PlayCountdown";
 
 export default class Play extends Component {
   render() {
     return (
       <Container>
         <Jumbotron>
-          <h2>PLAY THE GAME</h2>
-          <p>SPELL WORDS HERE</p>
+          <Row>
+            <Col>
+              <h2>The definition is:</h2>
+              <p>arrange or fold as a cover or protection</p>
+              <input type="text" />
+              <hr />
+              <PlayCountdown />
+            </Col>
+            <Col>
+              <h2>SCOREBOARD</h2>
+              <p>10 words correctly spelled in a row</p>
+              <hr />
+              <Row>
+                <Col>
+                  <h3 className="redStrike">X</h3>
+                </Col>
+                <Col>
+                  <h3 className="redStrike">X</h3>
+                </Col>
+                <Col>
+                  <h3 className="redStrike">X</h3>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
         </Jumbotron>
-        <Link to="/about">
-          <Button bsStyle="primary">About </Button>
-        </Link>
       </Container>
     );
   }
